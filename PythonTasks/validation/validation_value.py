288b5bf3ed47c9d1ce04aaafefdead073b292644
Aleksey.D <didilov.aleksey@gmail.com>
2019-02-24 11:05:57 +0200

    Package 'validation' has been created, this package contains: - empty __init__.py module - validation_value.py module which contains classes for ivalidation input value.

---
+++ PythonTasks/validation/validation_value.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Requirements(ABC):
+
+    @abstractmethod
+    def passed(self, value: Any) -> bool:
+        pass
+
+
+class GreaterThanZero(Requirements):
+
+    def passed(self, value: int) -> bool:
+        return value > 0
+
+
+class CheckRequirements(Requirements):
+
+    def __init__(self, *requirements: Any):
+        self._test = requirements
+
+    def passed(self, value: int) -> bool:
+        for requirement in self._test:
+            if not requirement.passed(value):
+                print(f"{requirement.__class__.__name__} is failed.")
+                return False
+        return True
