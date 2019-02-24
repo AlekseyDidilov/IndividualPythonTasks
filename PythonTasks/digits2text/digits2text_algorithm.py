ee122a89d6a53b85800d6b8a9c923fc560533708
Aleksey.D <didilov.aleksey@gmail.com>
2019-02-24 15:16:51 +0200

    Module digits2text_algorithm.py has been updated, GIGITS imported from numbers instead digits dictionary.

--- PythonTasks/digits2text/digits2text_algorithm.py
+++ PythonTasks/digits2text/digits2text_algorithm.py
@@ -1,4 +1,4 @@
-from digits2text.numbers import digits_dictionary
+from digits2text.numbers import DIGITS
 
 
 class TranslatorInText:
@@ -9,23 +9,23 @@ class TranslatorInText:
 
     def numbers_less_100(self, number: int) -> str:
         if 1 <= number <= 9:
-            self.output_text = digits_dictionary["units"][number]
+            self.output_text = DIGITS["units"][number]
         elif 10 <= number <= 19:
-            self.output_text = digits_dictionary["teen"][number]
+            self.output_text = DIGITS["teen"][number]
         elif number == 0:
-            self.output_text = digits_dictionary["units"][number]
+            self.output_text = DIGITS["units"][number]
         else:
-            self.output_text = digits_dictionary["tens"][number // 10] + \
-                               " " + digits_dictionary["units"][number % 10]
+            self.output_text = DIGITS["tens"][number // 10] + \
+                               " " + DIGITS["units"][number % 10]
         return self.output_text
 
     def numbers_from_100_to_999(self, number: int) -> str:
         if number <= 99:
             return self.numbers_less_100(number)
         if number % 100 == 0:
-            self.output_text += digits_dictionary["hundreds"][number // 100]
+            self.output_text += DIGITS["hundreds"][number // 100]
         else:
-            self.output_text += digits_dictionary["hundreds"][number // 100] + \
+            self.output_text += DIGITS["hundreds"][number // 100] + \
                                 " " + self.numbers_less_100(number % 100)
         return self.output_text
 
@@ -38,7 +38,7 @@ class TranslatorInText:
         else:
             digit = int((str(thousand_count))[-1])
             thousand = self.numbers_from_100_to_999(thousand_count) + " тыся" + \
-                       digits_dictionary["thousands_ends"][digit]
+                       DIGITS["thousands_ends"][digit]
 
         thousand = thousand.replace("один ", "одна ")
         thousand = thousand.replace("два ", "две ")
@@ -49,7 +49,7 @@ class TranslatorInText:
     def numbers_to_text(self) -> str:
         number = self.number
         if number == 0:
-            self.output_text += digits_dictionary["zero"][number]
+            self.output_text += DIGITS["zero"][number]
         elif 0 < number <= 999:
             self.output_text += self.numbers_from_100_to_999(number)
         elif 1 <= number // 1000 <= 999:
