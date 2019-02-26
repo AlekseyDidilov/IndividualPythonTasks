599a6cbe399bc46514250b17805e10ed76786dbc
Aleksey.D <didilov.aleksey@gmail.com>
2019-02-26 22:00:58 +0200

    Module chessboard_algorithm.py has been updated, variables length and width has got underlines which shows, that this variables will not be changed.

--- PythonTasks/models/chessboard_algorithm.py
+++ PythonTasks/models/chessboard_algorithm.py
@@ -1,13 +1,13 @@
 class Chessboard:
 
     def __init__(self, length: int, width: int):
-        self.length = length
-        self.width = width
+        self._length = length
+        self._width = width
 
     def chessboard_rendering(self) -> None:
-        for line_number in range(self.length):
+        for line_number in range(self._length):
             if line_number % 2 == 0:
-                print(("* " * (self.width // 2)) + ("*" * (self.width % 2)))
+                print(("* " * (self._width // 2)) + ("*" * (self._width % 2)))
             else:
-                print((" *" * (self.width // 2)) + (" " * (self.width % 2)))
+                print((" *" * (self._width // 2)) + (" " * (self._width % 2)))
 
