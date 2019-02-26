ecfdc98fed884ddc9c439282d6349cd2553a40b6
Aleksey.D <didilov.aleksey@gmail.com>
2019-02-26 22:09:29 +0200

    Module chessboard_algorithm.py has been updated, variables number and translated_text has got underlines which shows, that this variables will not be changed.

--- PythonTasks/models/digits2text_algorithm.py
+++ PythonTasks/models/digits2text_algorithm.py
@@ -4,30 +4,30 @@ from models.numbers import DIGITS
 class TranslatorInText:
 
     def __init__(self, numbers: int):
-        self.number = numbers
-        self.translated_text = ""
+        self._number = numbers
+        self._translated_text = ""
 
     def less_hundred(self, number: int) -> str:
         if 1 <= number <= 9:
-            self.translated_text = DIGITS["units"][number]
+            self._translated_text = DIGITS["units"][number]
         elif 10 <= number <= 19:
-            self.translated_text = DIGITS["teen"][number]
+            self._translated_text = DIGITS["teen"][number]
         elif number == 0:
-            self.translated_text = DIGITS["units"][number]
+            self._translated_text = DIGITS["units"][number]
         else:
-            self.translated_text = DIGITS["tens"][number // 10] + \
+            self._translated_text = DIGITS["tens"][number // 10] + \
                                " " + DIGITS["units"][number % 10]
-        return self.translated_text
+        return self._translated_text
 
     def hundreds(self, number: int) -> str:
         if number <= 99:
             return self.less_hundred(number)
         if number % 100 == 0:
-            self.translated_text += DIGITS["hundreds"][number // 100]
+            self._translated_text += DIGITS["hundreds"][number // 100]
         else:
-            self.translated_text += DIGITS["hundreds"][number // 100] + \
+            self._translated_text += DIGITS["hundreds"][number // 100] + \
                                 " " + self.less_hundred(number % 100)
-        return self.translated_text
+        return self._translated_text
 
     def thousands(self, number: int) -> str:
         divided_thousand = number // 1000
@@ -42,19 +42,19 @@ class TranslatorInText:
 
         thousand = thousand.replace("один ", "одна ")
         thousand = thousand.replace("два ", "две ")
-        self.translated_text = str()
-        self.translated_text += thousand + " " + self.hundreds(number % 1000)
-        return self.translated_text
+        self._translated_text = str()
+        self._translated_text += thousand + " " + self.hundreds(number % 1000)
+        return self._translated_text
 
     def numbers_to_text(self) -> str:
-        number = self.number
+        number = self._number
         if number == 0:
-            self.translated_text += DIGITS["zero"][number]
+            self._translated_text += DIGITS["zero"][number]
         elif 0 < number <= 999:
-            self.translated_text += self.hundreds(number)
+            self._translated_text += self.hundreds(number)
         elif 1 <= number // 1000 <= 999:
-            self.translated_text = self.thousands(number)
-        return self.translated_text
+            self._translated_text = self.thousands(number)
+        return self._translated_text
 
 
 
