ae45ea6b5dca958eeeb4effd37375a24692ed4fe
Aleksey.D <didilov.aleksey@gmail.com>
2019-02-27 22:01:23 +0200

    Module triangles_main has been created, this module contans objects which validatate input data, create new triagles and print sorted list with them.

---
+++ PythonTasks/services/triangles_main.py
@@ -0,0 +1,42 @@
+from models.validation_value import (
+    CheckRequirements, SidesFloat,
+    SumTwoSides, LessZero, StringLength
+)
+
+from models.input_value import AskInput, SplitInput
+from models.execution import NewExecution
+
+from models.triangles import Triangle, TriangleList
+
+
+if __name__ == '__main__':
+    triangles_sorted = TriangleList()
+    while True:
+        triangle_data = SplitInput(AskInput("Please, enter triangle name and size of sides \n"
+                                            "Input format - 'name, side_a, side_b, side_c':"
+                                            )).value()
+        checked_length = CheckRequirements(StringLength())
+        checked_sides = CheckRequirements(SidesFloat(), SumTwoSides(), LessZero())
+        if checked_length.passed(triangle_data) and \
+            checked_sides.passed(
+                                triangle_data[1],
+                                triangle_data[2],
+                                triangle_data[3]
+                                ):
+            new_triangle = Triangle(
+                                    str(triangle_data[0]),
+                                    float(triangle_data[1]),
+                                    float(triangle_data[2]),
+                                    float(triangle_data[3])
+                                    ).square_calculation()
+            triangles_sorted.add_triangle(new_triangle)
+        else:
+            break
+        run_again = NewExecution(
+                                 "Do you want add new triangle?\n"
+                                 "If yes, please type 'y' or 'yes'\n"
+                                ).choose_action()
+        if not run_again:
+            break
+    triangles_sorted.print_triangles()
+
