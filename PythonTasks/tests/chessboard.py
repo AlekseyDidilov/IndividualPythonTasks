c29a13a02ab4e17c9f15eeff65af07aca7cd9043
Aleksey.D <didilov.aleksey@gmail.com>
2019-03-06 23:13:07 +0200

    Module chessboard.py has been created: Module contains 3 test function, one of them verificates valid working of chessboard_building fuction. Second and third functions make sure that chessboard_building fucntion works properly with invalid output.

---
+++ PythonTasks/tests/chessboard.py
@@ -0,0 +1,36 @@
+from models.chessboard_algorithm import Chessboard
+
+
+class TestChessboard:
+
+    def test_chessboard_building_valid_output(self):
+        output_list = [
+            '* * *',
+            ' * * ',
+            '* * *',
+            ' * * ',
+            '* * *',
+            ]
+        chessboard_instance = Chessboard(5, 5).chessboard_building()
+        assert output_list == chessboard_instance
+
+    def test_chessboard_building_invalid_first_line(self):
+        output_list = [
+            ' * * ',
+            ' * * ',
+            '* * *',
+            ' * * ',
+            '* * *',
+            ]
+        chessboard_instance = Chessboard(5, 5).chessboard_building()
+        assert output_list != chessboard_instance
+
+    def test_chessboard_building_invalid_size(self):
+        output_list = [
+            ' * * ',
+            ' * * ',
+            '* * *',
+            ' * * ',
+            ]
+        chessboard_instance = Chessboard(5, 5).chessboard_building()
+        assert output_list != chessboard_instance
