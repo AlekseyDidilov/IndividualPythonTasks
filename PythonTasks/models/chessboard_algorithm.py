7329e0edb055210b2dd1eb8f417d969a11f78ee1
Aleksey.D <didilov.aleksey@gmail.com>
2019-03-06 22:40:06 +0200

    Module chessboard_algorithm.py has been modified: - Import List has been added to use as type hint in chessboard_rendering fuction. - Return in chessboard_rendering fuction has been changed from None -> List.

--- PythonTasks/models/chessboard_algorithm.py
+++ PythonTasks/models/chessboard_algorithm.py
@@ -1,13 +1,17 @@
+from typing import List
+
+
 class Chessboard:
 
     def __init__(self, length: int, width: int):
         self._length = length
         self._width = width
 
-    def chessboard_rendering(self) -> None:
+    def chessboard_building(self) -> List[str]:
+        chessboard_model = list()
         for line_number in range(self._length):
             if line_number % 2 == 0:
-                print(("* " * (self._width // 2)) + ("*" * (self._width % 2)))
+                chessboard_model.append(("* " * (self._width // 2)) + ("*" * (self._width % 2)))
             else:
-                print((" *" * (self._width // 2)) + (" " * (self._width % 2)))
-
+                chessboard_model.append((" *" * (self._width // 2)) + (" " * (self._width % 2)))
+        return chessboard_model
