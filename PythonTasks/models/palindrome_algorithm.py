ff27bb606d3e200a612c5ecb2fd9cb4ba286cfe3
Aleksey.D <didilov.aleksey@gmail.com>
2019-03-07 09:50:28 +0200

    Changes in module palindrome_algorithm: - Module name has been modified palindrome -> palindrome_algorithm. - Function was moved as methods in Palindrome class. - Validation, input amd main items has been moved in apropriated modules. Module palindrome_main has been created, this modul contain logic to create palindrome instance.

---
+++ PythonTasks/models/palindrome_algorithm.py
@@ -0,0 +1,36 @@
+from typing import Set, Any
+
+
+class Palindrome:
+
+    def __init__(self, value: str):
+        self._value = value
+
+    def is_palindrome(self) -> Any:
+        if self._value == self._value[::-1]:
+            return self._value
+        else:
+            return self.is_palindrome_as_part()
+
+    def is_palindrome_as_part(self) -> Set[Any]:
+        palindrome_list = list()
+        start_index, stop_index = 0, 2
+
+        for _ in range(len(self._value)-1):
+            for _ in range(len(self._value)-1):
+                slice_ = self._value[start_index:stop_index]
+                stop_index += 1
+                if slice_ == slice_[::-1]:
+                    palindrome_list.append(slice_)
+            start_index += 1
+            stop_index = start_index + 2
+
+        if len(palindrome_list) > 0:
+            palindrome_list = set(palindrome_list)
+            return palindrome_list
+
+        else:
+            palindrome_list.append(0)
+            palindrome_list = set(palindrome_list)
+            return palindrome_list
+
---
+++ PythonTasks/services/palindrome_main.py
@@ -0,0 +1,10 @@
+from models.input_value import AskInput
+from models.palindrome_algorithm import Palindrome
+
+
+if __name__ == "__main__":
+    required_value = AskInput("Please, enter required value:").value()
+    check_palindrome = Palindrome(required_value).is_palindrome()
+    print(f"After checking, your palindrome is: {check_palindrome}")
+
+
